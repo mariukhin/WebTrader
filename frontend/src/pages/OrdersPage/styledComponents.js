fb783541a921b2cfb3502c269c1e0ecfdaee751c
m.mariukhin <m.mariukhin@amifactory.team>
2021-04-23 17:53:06 +0300

    feat: implement OrdersPage design

--- frontend/src/pages/OrdersPage/styledComponents.js
+++ frontend/src/pages/OrdersPage/styledComponents.js
@@ -1,4 +1,8 @@
+import { Checkbox, Input, Typography, Button } from 'antd';
 import styled from 'styled-components';
+
+const { Text } = Typography;
+const { Search } = Input;
 
 export const Wrapper = styled.main`
   display: grid;
@@ -9,7 +13,30 @@ export const Wrapper = styled.main`
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  padding: 20px;
   height: 80vh;
-  justify-content: center;
-  text-align: center;
+`;
+
+export const ShowTradersCheckbox = styled(Checkbox)`
+  width: 15%;
+  margin-bottom: 20px;
+`;
+
+export const FilterContainer = styled.div`
+  display: flex;
+  margin-bottom: 15px;
+`;
+
+export const FilterInput = styled(Search)`
+  width: 25%;
+  margin-left: 10px;
+`;
+
+export const FilterText = styled(Text)`
+  line-height: 30px;
+`;
+
+export const ResetButton = styled(Button)`
+  margin-left: 10px;
+  border-radius: 3px;
 `;
